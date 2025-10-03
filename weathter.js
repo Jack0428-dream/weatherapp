44b3ea10d40878aa5e463f6a1b98c584cd48309c
Jack0428-dream <192861219+Jack0428-dream@users.noreply.github.com>
2025-10-03 22:32:40 +0900

    process JSON and set up form

--- weathter.js
+++ weathter.js
@@ -2,20 +2,29 @@
 // fun -> that take a location and return the weather data 
 // -> location variable -> api url + location (using literal format)
 // for that location
+const text = document.querySelector('#location');
+const button = document.querySelector('button');
 
 function weatherJson() {
-    let location = prompt("Location?", '');
+    let location = text.value;
+    const div = document.createElement('div');
 
     fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=4HZ9NFLDENKLDNGQJ35K9THY5`)
         .then(function(response) {
             return response.json();
         })
         .then(function(response) {
-            console.log(response.currentConditions);
+            console.log(response.days);
         })
         .catch(function(err) {
             console.log(err);
         })
 }
 
-weatherJson();
+
+button.addEventListener('click', () => {
+    weatherJson();
+    text.value = "";
+})
+
+// weatherJson();
